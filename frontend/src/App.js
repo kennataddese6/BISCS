4969a949e6b4643582fc7fcb5e46d4f360d4be99
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-12 18:44:29 +0300

    add love message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="Introduction">
       {" "}
       <h1>Barcode Integrated Student clearance system(BISCS).</h1>
+      <h1>I love you very much my Love. I love you forever</h1>
     </div>
   );
 }
