27b8294b11ea8a7fd579d2c039c437f6faa8256f
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-12 17:50:31 +0300

    create a frontend and backend for project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Introduction">
+      {" "}
+      <h1>Barcode Integrated Student clearance system(BISCS).</h1>
+    </div>
+  );
+}
+
+export default App;
