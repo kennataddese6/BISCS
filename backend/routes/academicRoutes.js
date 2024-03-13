07f083c6ef195a0fd5ba7aaa2972025603180c0d
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-13 16:37:26 +0300

    create a route to get all academic types

--- backend/routes/academicRoutes.js
+++ backend/routes/academicRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createAcademicType } = require("../controllers/academicTypeController");
+const {
+  createAcademicType,
+  getAcademicType,
+} = require("../controllers/academicTypeController");
 
 router.post("/", createAcademicType); // Create a route or opening door to create academic type
+router.get("/", getAcademicType);
 
 module.exports = router;
