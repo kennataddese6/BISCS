9e20fc627a3320f573c14348a86e1c47f1551026
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-13 18:00:53 +0300

    add a sub container for each step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,25 @@ function App() {
   return (
     <>
       <div className="stepContainer">
-        <div className="stepCircle"> 1 </div>
-        <div className="stepText"> Academic </div>
-        <div className="stepLine"> </div>
-        <div className="stepCircle"> 2 </div>
-        <div className="stepText"> Clearance </div>
-        <div className="stepLine"> </div>
-        <div className="stepCircle"> 3 </div>
-        <div className="stepText"> Define rule </div>
-        <div className="stepLine"> </div>
-        <div className="stepCircle"> 4 </div>
-        <div className="stepText"> Complete </div>
+        <div className="subStepContainer">
+          <div className="stepCircle"> 1 </div>
+          <div className="stepText"> Academic </div>
+          <div className="stepLine"> </div>
+        </div>
+        <div className="subStepContainer">
+          <div className="stepCircle"> 2 </div>
+          <div className="stepText"> Clearance </div>
+          <div className="stepLine"> </div>
+        </div>
+        <div className="subStepContainer">
+          <div className="stepCircle"> 3 </div>
+          <div className="stepText"> Define rule </div>
+          <div className="stepLine"> </div>
+        </div>
+        <div className="subStepContainer">
+          <div className="stepCircle"> 4 </div>
+          <div className="stepText"> Complete </div>
+        </div>
       </div>
     </>
   );
