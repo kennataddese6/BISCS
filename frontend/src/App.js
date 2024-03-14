3941e19676382f3ee24f56cf2e59ec3999ca2edf
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-14 14:07:38 +0300

    remove the step line from the substep container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,18 @@ function App() {
         <div className="subStepContainer">
           <div className="stepCircle"> 1 </div>
           <div className="stepText"> Academic </div>
-          <div className="stepLine"> </div>
         </div>
+        <div className="stepLine"> </div>
         <div className="subStepContainer">
           <div className="stepCircle inactiveStep"> 2 </div>
           <div className="stepText inactiveText"> Clearance </div>
-          <div className="stepLine inactiveStep"> </div>
         </div>
+        <div className="stepLine inactiveStep"> </div>
         <div className="subStepContainer">
           <div className="stepCircle inactiveStep"> 3 </div>
           <div className="stepText inactiveText"> Define rule </div>
-          <div className="stepLine inactiveStep"> </div>
         </div>
+        <div className="stepLine inactiveStep"> </div>
         <div className="subStepContainer">
           <div className="stepCircle inactiveStep"> 4 </div>
           <div className="stepText inactiveText"> Complete </div>
