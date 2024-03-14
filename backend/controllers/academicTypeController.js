e1fd447695359bb494af77dd563d8f48b4327200
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-14 18:47:52 +0300

    add a loop to loop over an array and register

--- backend/controllers/academicTypeController.js
+++ backend/controllers/academicTypeController.js
@@ -4,9 +4,15 @@ const asyncHandler = require("express-async-handler");
 //This is a function to create/register academic type
 const createAcademicType = asyncHandler(async (req, res) => {
   // It takes request from the frontend (req)
-  const result = await AcademicType.create({
-    AcademicName: req.body.AcademicName,
-  });
+  let result;
+  const AcademicTypes = req.body;
+  AcademicTypes &&
+    AcademicTypes.map(async (academicType) => {
+      result = await AcademicType.create({
+        AcademicName: academicType,
+      });
+    });
+
   if (result) {
     res.status(200).json("Your request was successful. Academic type created");
   } else {
