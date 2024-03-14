3e89bc622b2c0f0e400a655b4fdb8b9fc71e108b
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-14 16:45:04 +0300

    add more responsive ness to step header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,17 +26,17 @@ function App() {
           <div className="stepCircle"> 1 </div>
           <div className="stepText"> Academic </div>
         </div>
-        <div className="stepLine col-s-3 col-l-1"> </div>
+        <div className="stepLine col-s-1 col-l-1 col-m-1"> </div>
         <div className="subStepContainer">
           <div className="stepCircle inactiveStep"> 2 </div>
           <div className="stepText inactiveText"> Clearance </div>
         </div>
-        <div className="stepLine inactiveStep col-s-3 col-l-1"> </div>
+        <div className="stepLine inactiveStep col-s-1 col-l-1 col-m-1"> </div>
         <div className="subStepContainer">
           <div className="stepCircle inactiveStep "> 3 </div>
           <div className="stepText inactiveText"> Define rule </div>
         </div>
-        <div className="stepLine inactiveStep col-s-3 col-l-1"> </div>
+        <div className="stepLine inactiveStep col-s-1 col-l-1 col-m-1"> </div>
         <div className="subStepContainer">
           <div className="stepCircle inactiveStep"> 4 </div>
           <div className="stepText inactiveText"> Complete </div>
