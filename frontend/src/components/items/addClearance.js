12c111fad0b99411a94f9d89aa8d48f5afaf4769
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-15 17:50:26 +0300

    add a container for displaying added clearancenames

--- frontend/src/components/items/addClearance.js
+++ frontend/src/components/items/addClearance.js
@@ -2,11 +2,24 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAcademicTypes } from "../../features/academicType/academicSlice";
 import "./styles/addclearance.css";
+import { useState } from "react";
+import { MdClose } from "react-icons/md";
+
 const AddClearance = () => {
   const dispatch = useDispatch();
+  const [clearanceNames, setClearanceNames] = useState([
+    "Dormitory",
+    "Cafteria",
+    "Finance",
+    "Department",
+  ]);
   const { isError, isSuccess, message } = useSelector(
     (state) => state.academic
   );
+  const handleRemoveAcademic = (value) => {
+    const newclearancName = clearanceNames.filter((a) => a !== value);
+    setClearanceNames(newclearancName);
+  };
   useEffect(() => {
     dispatch(getAcademicTypes());
   }, [dispatch]);
@@ -16,10 +29,29 @@ const AddClearance = () => {
   return (
     <>
       <div className="addClearanceContainer">
-        <div className="clearance col-l-4">Container 1</div>
-        <div className="clearance col-l-4">Container 2</div>
-        <div className="clearance col-l-4">Container 3</div>
-        <div className="clearance col-l-4">Container 4</div>
+        {message &&
+          message.map((AcademicNames) => (
+            <div className="clearance col-l-4 ">
+              <h3 className="addClearanceHeaderText">
+                Add clearance for {AcademicNames.AcademicName}
+              </h3>
+              <div className="clearancetypesContainer">
+                {clearanceNames.map((clearanceName, index) => (
+                  <div key={index} className="clearanceType col-s-4  col-xl-3">
+                    <div className="academicTypeText">{clearanceName}</div>
+                    <div
+                      className="closeIconContainer"
+                      onClick={() => {
+                        handleRemoveAcademic(clearanceName);
+                      }}
+                    >
+                      <MdClose />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
       </div>
       <div className="nextButtonContainer">
         <button className="nextStepButton"> Next </button>
