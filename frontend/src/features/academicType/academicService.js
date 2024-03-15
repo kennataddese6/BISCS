09ac8f071ccca33d0cac833e2e49d7f0f09ff17a
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-15 16:31:55 +0300

    create a service for getting academic types from api

--- frontend/src/features/academicType/academicService.js
+++ frontend/src/features/academicType/academicService.js
@@ -3,12 +3,17 @@ const API_URL = "http://localhost:5000/api/";
 
 const createAcademicType = async (academicNames) => {
   const response = await axios.post(API_URL + "academicType", academicNames);
+  return response.data;
+};
+const getAcademicTypes = async () => {
+  const response = await axios.post(API_URL + "academicType");
   console.log(response);
   return response.data;
 };
 
 const academicService = {
   createAcademicType,
+  getAcademicTypes,
 };
 
 export default academicService;
