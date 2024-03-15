cfe602da4864e11dd01810d356ff4ff4ebd9039b
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-15 16:46:40 +0300

    dispatch and get all registerd academic types

--- frontend/src/components/items/addClearance.js
+++ frontend/src/components/items/addClearance.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAcademicTypes } from "../../features/academicType/academicSlice";
+
 const AddClearance = () => {
+  const dispatch = useDispatch();
+  const { isError, isSuccess, message } = useSelector(
+    (state) => state.academic
+  );
+  useEffect(() => {
+    dispatch(getAcademicTypes());
+  }, [dispatch]);
+  useEffect(() => {
+    console.log(isError, isSuccess, message);
+  }, [isSuccess, isError, message]);
   return (
     <>
       <div style={{ color: "white" }}>
