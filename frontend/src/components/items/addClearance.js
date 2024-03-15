1f5cf6ec356153c30a23e9228cfc276fb8302520
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-15 17:00:18 +0300

    create container for adding clearance to academic types

--- frontend/src/components/items/addClearance.js
+++ frontend/src/components/items/addClearance.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAcademicTypes } from "../../features/academicType/academicSlice";
-
+import "./styles/addclearance.css";
 const AddClearance = () => {
   const dispatch = useDispatch();
   const { isError, isSuccess, message } = useSelector(
@@ -15,8 +15,11 @@ const AddClearance = () => {
   }, [isSuccess, isError, message]);
   return (
     <>
-      <div style={{ color: "white" }}>
-        Add Clearance for this academic types
+      <div className="addClearanceContainer">
+        <div className="clearance col-l-4">Container 1</div>
+        <div className="clearance col-l-4">Container 2</div>
+        <div className="clearance col-l-4">Container 3</div>
+        <div className="clearance col-l-4">Container 4</div>
       </div>
       <div className="nextButtonContainer">
         <button className="nextStepButton"> Next </button>
