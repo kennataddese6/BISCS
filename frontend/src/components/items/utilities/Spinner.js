26da580fd667ccae0d0b874878b42e154905f637
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-15 14:49:24 +0300

    create a spinner

---
+++ frontend/src/components/items/utilities/Spinner.js
@@ -0,0 +1,10 @@
+import "../styles/spinner.css";
+const Spinner = () => {
+  return (
+    <div className="spinnerContainer">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
