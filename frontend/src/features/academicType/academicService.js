a98f8e0cb9473b41ad8c164c50459073219aec97
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-15 14:10:34 +0300

    return response data in service

--- frontend/src/features/academicType/academicService.js
+++ frontend/src/features/academicType/academicService.js
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:5000/api/";
 const createAcademicType = async (academicNames) => {
   const response = await axios.post(API_URL + "academicType", academicNames);
   console.log(response);
-  return response;
+  return response.data;
 };
 
 const academicService = {
