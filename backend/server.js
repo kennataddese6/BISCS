e87ca8be7f956259071bb67b6b1dd34ea0b21507
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-18 10:43:46 +0300

    use the new clearance route

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/api/academicType", require("./routes/academicRoutes"));
+app.use("/api/clearanceType", require("./routes/academicRoutes"));
 
 const server = app.listen(port, () =>
   console.log(`Server started on port ${port}`)
