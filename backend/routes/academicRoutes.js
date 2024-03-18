57e9805dffb1dd4d6043d8d491a1ac0630be861a
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-18 10:43:13 +0300

    change route to clearance route

--- backend/routes/academicRoutes.js
+++ backend/routes/academicRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  createAcademicType,
-  getAcademicType,
+  createClearanceType,
+  getClearanceType,
 } = require("../controllers/academicTypeController");
 
-router.post("/", createAcademicType); // Create a route or opening door to create academic type
-router.get("/", getAcademicType);
+router.post("/", createClearanceType); // Create a route or opening door to create academic type
+router.get("/", getClearanceType);
 
 module.exports = router;
