aacac2a3711069e57d3758e1ba6684425e1922f1
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-18 10:44:29 +0300

    change the api url to clearnce

--- frontend/src/features/academicType/academicService.js
+++ frontend/src/features/academicType/academicService.js
@@ -2,11 +2,11 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/";
 
 const createAcademicType = async (academicNames) => {
-  const response = await axios.post(API_URL + "academicType", academicNames);
+  const response = await axios.post(API_URL + "clearanceType", academicNames);
   return response.data;
 };
 const getAcademicTypes = async () => {
-  const response = await axios.get(API_URL + "academicType");
+  const response = await axios.get(API_URL + "clearanceType");
   return response.data;
 };
 
