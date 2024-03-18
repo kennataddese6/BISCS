8737357a1c47094e920c0441177bbfd3941815c9
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-18 10:57:46 +0300

    rename academic functions to clearance

--- frontend/src/features/academicType/academicService.js
+++ frontend/src/features/academicType/academicService.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 const API_URL = "http://localhost:5000/api/";
 
-const createAcademicType = async (academicNames) => {
+const createClearanceType = async (academicNames) => {
   const response = await axios.post(API_URL + "clearanceType", academicNames);
   return response.data;
 };
-const getAcademicTypes = async () => {
+const getClearanceTypes = async () => {
   const response = await axios.get(API_URL + "clearanceType");
   return response.data;
 };
 
 const academicService = {
-  createAcademicType,
-  getAcademicTypes,
+  createClearanceType,
+  getClearanceTypes,
 };
 
 export default academicService;
