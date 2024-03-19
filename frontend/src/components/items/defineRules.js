9b82527f0d208204d89cdca5a1bb0d7e0f881703
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-19 11:51:22 +0300

    dispatch and get all clearancetypes and display them

--- frontend/src/components/items/defineRules.js
+++ frontend/src/components/items/defineRules.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles/definerules.css";
-
+import { getClearanceTypes } from "../../features/academicType/clearanceSlice";
+import { useDispatch, useSelector } from "react-redux";
 const DefineRules = () => {
+  const dispatch = useDispatch();
+  const { message, isSuccess } = useSelector((state) => state.clearance);
   const [Divs, setDivs] = useState(["A", "B", "C", "D"]);
-
   const handleDragStart = (index) => (event) => {
     event.dataTransfer.setData("index", index);
   };
@@ -18,23 +20,49 @@ const DefineRules = () => {
       setDivs(newDiv);
     }
   };
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(message);
+    }
+  }, [isSuccess, message]);
+  useEffect(() => {
+    dispatch(getClearanceTypes());
+  }, [dispatch]);
   return (
-    <div className="academicContainer">
-      {Divs.map((content, index) => (
-        <div
-          className="academicType col-s-3"
-          key={index}
-          data-index={index}
-          draggable={true}
-          onDragStart={handleDragStart(index)}
-          onDragOver={(e) => e.preventDefault()}
-          onDrop={handleDrop}
-        >
-          {" "}
-          {content}{" "}
-        </div>
-      ))}
-    </div>
+    <>
+      {" "}
+      <div className="addClearanceContainer">
+        {message &&
+          message.map((AcademicNames, index) => (
+            <div className="clearance col-l-4 ">
+              <h3 className="addClearanceHeaderText">
+                Define for {AcademicNames.AcademicName} clearance
+              </h3>
+              <label className="reorderclearance"> Reorder Clearance</label>
+              <div className="clearancetypesContainer">
+                {Divs.map((content, index) => (
+                  <div
+                    className="academicType draggable col-s-3"
+                    key={index}
+                    data-index={index}
+                    draggable={true}
+                    onDragStart={handleDragStart(index)}
+                    onDragOver={(e) => e.preventDefault()}
+                    onDrop={handleDrop}
+                  >
+                    {" "}
+                    {content}{" "}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        <div className="nextButtonContainer"></div>
+      </div>
+      <div className="nextButtonContainer">
+        <button className="nextStepButton"> Next </button>
+      </div>
+    </>
   );
 };
 
