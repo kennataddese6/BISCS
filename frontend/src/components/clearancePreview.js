b9d7083bda18d12c00127c0eeeae1bc473ce3f24
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-21 12:06:59 +0300

    create a component for preview

---
+++ frontend/src/components/clearancePreview.js
@@ -0,0 +1,12 @@
+const ClearancePreview = () => {
+  return (
+    <>
+      <div> Preview Page</div>
+      <div className="nextButtonContainer">
+        <button className="nextStepButton"> Finish </button>
+      </div>
+    </>
+  );
+};
+
+export default ClearancePreview;
