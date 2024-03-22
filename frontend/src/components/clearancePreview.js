288837f506720b6cf3c2b46368fa389c636a822a
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-22 09:17:17 +0300

    get all clearance types and display

--- frontend/src/components/clearancePreview.js
+++ frontend/src/components/clearancePreview.js
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+import { getClearanceTypes } from "../features/academicType/clearanceSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const ClearancePreview = () => {
+  const { isSuccess, message } = useSelector((state) => state.clearance);
+  const [Clearances, setClearance] = useState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isSuccess) {
+      setClearance(message);
+    }
+  }, [isSuccess, message]);
+  useEffect(() => {
+    dispatch(getClearanceTypes());
+  }, [dispatch]);
   return (
     <>
-      <div> Preview Page</div>
+      <div className="addClearanceContainer">
+        {Clearances &&
+          Clearances.map((clearance, index) => (
+            <div className="clearance col-l-4">
+              <h3 className="addClearanceHeaderText">
+                {" "}
+                {clearance.AcademicName} Clearance
+              </h3>
+            </div>
+          ))}
+      </div>
       <div className="nextButtonContainer">
         <button className="nextStepButton"> Finish </button>
       </div>
