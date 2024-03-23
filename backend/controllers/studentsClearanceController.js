89e42f440daa5f3e860b1cc76f29380dbd7ab48c
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-23 16:56:02 +0300

    fix function to only create one clearance for one student

--- backend/controllers/studentsClearanceController.js
+++ backend/controllers/studentsClearanceController.js
@@ -6,18 +6,24 @@ const asyncHandler = require("express-async-handler");
 const createStudentsClearance = asyncHandler(async (req, res) => {
   const clearances = await Clearance.find();
   const students = await Students.find();
-
-  for (const student of students) {
-    for (const clearance of clearances) {
-      if (student.EnrolledIn === clearance.AcademicName)
-        await StudentsClearance.create({
-          AcademicName: clearance.AcademicName,
-          StudentId: student.StudentId,
-          AdminId: student.AdminId,
-          Deadline: clearance.Deadline,
-          ClearanceDetail: clearance.ClearanceDetail,
-        });
+  try {
+    for (const student of students) {
+      for (const clearance of clearances) {
+        if (student.EnrolledIn === clearance.AcademicName)
+          await StudentsClearance.create({
+            AcademicName: clearance.AcademicName,
+            StudentId: student.StudentId,
+            AdminId: student.AdminId,
+            Deadline: clearance.Deadline,
+            ClearanceDetail: clearance.ClearanceDetail,
+          });
+        continue;
+      }
     }
+    const allStudentClearances = await StudentsClearance.find();
+    res.status(200).json(allStudentClearances);
+  } catch (error) {
+    res.status(400).json(error);
   }
 });
 
