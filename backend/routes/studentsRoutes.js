fa702cbaee2f0be44f8824b281106cc8f4dc6ba3
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-23 11:37:49 +0300

    create a route for students

---
+++ backend/routes/studentsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { createStudent } = require("../controllers/studentController");
+
+router.post("/", createStudent);
+
+module.exports = router;
