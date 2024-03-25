b49b836b2a073417df360a68c475a903da7fcdac
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-25 14:10:18 +0300

    add clearance to a route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,23 @@
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Steps from "./components/items/steps";
 import Spinner from "./components/items/utilities/Spinner";
-import ClearanceTypes from "./components/items/clearanceTypes";
-import AddClearance from "./components/items/addClearance";
-import DefineRules from "./components/items/defineRules";
-import ClearancePreview from "./components/clearancePreview";
+import Clearance from "./components/pages/Clearance";
+
 function App() {
   const { isLoading } = useSelector((state) => state.clearance);
-  const [stepNumber, setStepNumber] = useState(1);
 
   return (
     <>
       <ToastContainer theme="dark" />
       {isLoading && <Spinner />}
-
-      <div className="mainContainer">
-        <Steps stepNumber={stepNumber} />
-        {stepNumber === 1 && <ClearanceTypes setStepNumber={setStepNumber} />}
-        {stepNumber === 2 && <AddClearance setStepNumber={setStepNumber} />}
-        {stepNumber === 3 && <DefineRules setStepNumber={setStepNumber} />}
-        {stepNumber === 4 && <ClearancePreview setStepNumber={setStepNumber} />}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Clearance />} />
+        </Routes>
+      </Router>
     </>
   );
 }
