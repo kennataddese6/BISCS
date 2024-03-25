4ddd1d3db21aa9a6b76fb96cf24f2ff8db0b6876
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-25 16:06:05 +0300

    generate qrcode

--- frontend/src/components/pages/StudentProfile.js
+++ frontend/src/components/pages/StudentProfile.js
@@ -1,10 +1,13 @@
 import "../items/styles/studentprofile.css";
+import QRCode from "react-qr-code";
 const StudentProfile = () => {
   return (
     <>
       <div className="profileHeaderContainer col-l-8">
-        <div className="BarcodeContainer"> </div>
-        <div className="studentProfileContainer">P </div>
+        <div className="BarcodeContainer">
+          <QRCode value="65febe6416b86fb8a891f1a9" />{" "}
+        </div>
+        <div className="studentProfileContainer">K </div>
       </div>
     </>
   );
