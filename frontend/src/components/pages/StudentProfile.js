f412a73e0d1749b0fd2487b35a6896adec0560be
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-25 15:16:52 +0300

    create a profile container and a barcod holder in a new component

---
+++ frontend/src/components/pages/StudentProfile.js
@@ -0,0 +1,12 @@
+import "../items/styles/studentprofile.css";
+const StudentProfile = () => {
+  return (
+    <>
+      <div className="profileHeaderContainer col-l-8">
+        <div className="BarcodeContainer"> </div>
+        <div className="studentProfileContainer">P </div>
+      </div>
+    </>
+  );
+};
+export default StudentProfile;
