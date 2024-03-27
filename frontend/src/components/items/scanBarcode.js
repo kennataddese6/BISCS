bb579e634bbba7be3dd24cebb054c2c352ce0810
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-27 13:48:49 +0300

    create a compnent for scanning barcode

--- frontend/src/components/items/scanBarcode.js
+++ frontend/src/components/items/scanBarcode.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import { Scanner } from "@yudiel/react-qr-scanner";
 const ScanBarcode = () => {
+  const [scanResult, setScanResult] = useState("");
+  const handleResult = (text, result) => {
+    setScanResult(text + result);
+  };
   return (
     <>
-      <h3>Loading barcode scanner...</h3>
+      <div style={{ width: "200px", height: "200px" }}>
+        <Scanner
+          onResult={(text, result) => handleResult(text, result)}
+          onError={(error) => console.log(error?.message)}
+        />
+        <h3>Result: {scanResult}</h3>
+      </div>
     </>
   );
 };
