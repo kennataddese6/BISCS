2f8a000c3bcb06dc016dad6765a942857e6ea0ef
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-27 17:30:38 +0300

    dispatch and get all clearanc types

--- frontend/src/components/pages/InitiateClearance.js
+++ frontend/src/components/pages/InitiateClearance.js
@@ -1,13 +1,24 @@
 import "../items/styles/initiateclearance.css";
+import { getClearanceTypes } from "../../features/academicType/clearanceSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 const InitiateClearance = () => {
+  const dispatch = useDispatch();
+  const { message } = useSelector((state) => state.clearance);
+
+  useEffect(() => {
+    dispatch(getClearanceTypes());
+  }, [dispatch]);
   return (
     <>
       <h1 className="initateClearanceHeader">Initiate Clearance</h1>
       <div className="initateClearanceContainer col-m-8">
-        <div className="initiationClearances col-s-5 col-l-3"></div>
-        <div className="initiationClearances col-s-5 col-l-3"></div>
-        <div className="initiationClearances col-s-5 col-l-3"></div>
-        <div className="initiationClearances col-s-5 col-l-3"></div>
+        {message &&
+          message.map((clearance) => (
+            <div className="initiationClearances col-s-5 col-l-3">
+              {clearance.AcademicName}
+            </div>
+          ))}
         <div className="initiationClearances col-s-5 col-l-3">Student</div>
         <div className="initiationClearances col-s-5 col-l-3">All</div>
       </div>
