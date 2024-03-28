73092dab7a50c6c7197ff19ba82c89d0b014312d
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-28 13:54:12 +0300

    change the api to the forwarded port

--- frontend/src/features/academicType/clearanceService.js
+++ frontend/src/features/academicType/clearanceService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/";
+const API_URL = "https://vgf59b03-5000.uks1.devtunnels.ms/api/";
 
 const createClearanceType = async (academicNames) => {
   const response = await axios.post(API_URL + "clearanceType", academicNames);
